353c4d6d1060e0eaf5a94093a799d1acb7a52b3a
Anupam Kapoor <anupam.kapoor@gmail.com>
2025-01-26 16:24:14 +0530

    Update generated images via a single target
    A convenience target 'update_generated_images' allows us to:
    
       - convert ppm image to png format, and
       - generate a low-resolution version of the same image
    
    This makes it much more easier to keep the README.org in sync with
    what is actually happening.

--- src/executables/update_generated_images.sh
+++ src/executables/update_generated_images.sh
@@ -15,9 +15,9 @@ function show_usage() {
     echo "-----------------------------------------------------------------------------"
     echo "About : "
     echo "       '"${PROGRAM_NAME}"' update generated images for README.org"
+    echo "            -g actually perform the update"
+    echo "            -h show this help message"
     echo "-----------------------------------------------------------------------------"
-
-    exit
 }
 
 # ------------------------------------------------------------------------------
@@ -57,24 +57,38 @@ function error_printf() {
 # format, and also creates a 'low-resolution' version of the same
 # image as well.
 function update_all_images() {
+    local dest_dir=$1
+
     # --------------------------------------------------------------------------
     # what does the low-resolution image dimensions look like ?
     local lowres_dim='320x256'
 
     # --------------------------------------------------------------------------
-    # low resolution images will be named as 'image-foo-lowres.png'
-    # for a high resolution image called 'image-foo.png'
+    # a high resolution image, f.e. 'image-foo.png', will be named
+    # 'image-foo-lowres.png'.
+    #
+    # these images i.e. 'image-foo.png' and 'image-foo-lowres.png'
+    # both need to be placed in '$PROJECT_ROOT/generated-images'
+    # directory, and pushed into the repository.
     local lowres_img_suffix='lowres'
 
     ls *.ppm |
         while read img_fname_with_ext
         do
+            echo " updating '"${img_fname_with_ext}"'"
+
             local img_fname_no_ext=$(echo "${img_fname_with_ext%.*}")
 
             # ------------------------------------------------------------------
             # perform the conversion
             magick ${img_fname_with_ext} ${img_fname_no_ext}.png
             magick ${img_fname_with_ext} -resize ${lowres_dim} ${img_fname_no_ext}-${lowres_img_suffix}.png
+
+            # ------------------------------------------------------------------
+            # and copy the generated targets
+            cp ${img_fname_no_ext}.png ${dest_dir}
+            cp ${img_fname_no_ext}-${lowres_img_suffix}.png ${dest_dir}
+
         done
 }
 
@@ -84,23 +98,46 @@ function update_all_images() {
 function main() {
 
     # --------------------------------------------------------------------------
-    # usage is messed up
-    if [ $# -lt 1 ]
+    # lets go
+    while getopts gd:h flag
+    do
+        case "${flag}" in
+            d)
+                dest_dir_flag=1
+                dest_dir_value=${OPTARG}
+                ;;
+
+            g)
+                do_update_flag=1
+                do_update_value=1
+                ;;
+
+            h)
+                echo "wow !"
+                show_usage
+                ;;
+        esac
+    done
+
+    # --------------------------------------------------------------------------
+    # update flag must be specified
+    if [ "${do_update_value}" != "1" ]
     then
+        error_printf "not sure what to do ?"
         show_usage
+        return 254
     fi
 
     # --------------------------------------------------------------------------
-    # lets go
-    case "$1" in
-        "-g")
-            update_all_images
-        ;;
+    # null || bad-path is an error
+    if [[ ( -z "${dest_dir_value}" ) || ( ! -w ${dest_dir_value} ) ]]
+    then
+        error_printf "bad dest-dir:'"${dest_dir_value}"'"
+        show_usage
+        return 255
+    fi
 
-        "-h" | *)
-            show_usage
-            ;;
-    esac
+    update_all_images ${dest_dir_value}
 }
 
 # oh boy, oh boy, oh boy !
